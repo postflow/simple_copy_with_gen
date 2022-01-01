e55f21c35ce08510101c0446c43e321c79617d92
Afanasov S <you@example.com>
2022-01-01 23:29:38 +0300

    .

--- lib/builder.dart
+++ lib/builder.dart
@@ -3,11 +3,11 @@ import 'package:source_gen/source_gen.dart';
 import 'src/extension_generator.dart';
 import 'src/subclass_generator.dart';
 
-Builder generateExtension(BuilderOptions options) =>
-    SharedPartBuilder([ExtensionGenerator()], 'extension_generator');
-
-// Builder copywith(BuilderOptions options) =>
+// Builder generateExtension(BuilderOptions options) =>
 //     SharedPartBuilder([ExtensionGenerator()], 'extension_generator');
+
+Builder copywith(BuilderOptions options) =>
+    SharedPartBuilder([ExtensionGenerator()], 'extension_generator');
 
 Builder generateSubclass(BuilderOptions options) =>
     SharedPartBuilder([SubclassGenerator()], 'subclass_generator');
