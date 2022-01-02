bd8228a63007bbe5aab41912e6003a2eb20df81c
Afanasov S <you@example.com>
2022-01-03 00:35:46 +0300

    fix test

--- test/test_lib.dart
+++ test/test_lib.dart
@@ -1,19 +1,22 @@
-
 import 'package:test/test.dart';
-
 import 'profile_model.dart';
 
-
-
-
 void main() {
-  test('Counter value should be incremented', () {
+  test('different field', () {
     final ProfileModel profileModel = ProfileModel()..name = 'Sergio';
-
     final newProfileModel = profileModel.copyWith(name: 'Michael');
-
     expect(true, profileModel.name != newProfileModel.name);
+  });
+
+  test('different hashCode', () {
+    final ProfileModel profileModel = ProfileModel()..name = 'Sergio';
+    final newProfileModel = profileModel.copyWith(name: 'Michael');
     expect(true, profileModel.hashCode != newProfileModel.hashCode);
+  });
+
+  test('different equals', () {
+    final ProfileModel profileModel = ProfileModel()..name = 'Sergio';
+    final newProfileModel = profileModel.copyWith(name: 'Michael');
     expect(true, profileModel != newProfileModel);
   });
 }
