d459e5122dc8d285623830efaa5f800635664501
Afanasov S <you@example.com>
2022-01-03 16:54:01 +0300

    fix package name

--- lib/src/subclass_generator.dart
+++ lib/src/subclass_generator.dart
@@ -1,6 +1,6 @@
 import 'package:build/src/builder/build_step.dart';
 import 'package:analyzer/dart/element/element.dart';
-import 'package:simple_copy_with_annotations/copy_with_annotations.dart';
+import 'package:simple_copy_with_annotations/simple_copy_with_annotations.dart';
 import 'package:source_gen/source_gen.dart';
 
 import 'model_visitor.dart';
